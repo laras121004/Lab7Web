dae4d0ccb635516cef5f50e91caf1ec29a2c9342
laras121004 <larassakti167@gmail.com>
2025-04-15 18:02:27 -0700

    file

---
+++ app/Views/contact.php
@@ -0,0 +1,11 @@
+<?= $this->include('template/header'); ?>
+
+<h2>Kontak Kami</h2>
+
+<div>
+    <p><strong>Email:</strong> <a href="mailto:laras61@gmail.com">laras61@gmail.com</a></p>
+    <p><strong>Alamat:</strong> Karawang, Cikarang Selatan</p>
+    <p><strong>Telepon:</strong> <a href="tel:+6281992064654">+62 819-9206-4654</a></p>
+</div>
+
+<?= $this->include('template/footer'); ?>
